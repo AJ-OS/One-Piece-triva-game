8238e319d2138128349890d8ca414da0a0c0277e
AJ Morris <112923316+AJ-OS@users.noreply.github.com>
2022-10-12 22:16:49 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,28 @@
+// changes color of "luffy button" to greena after being clicked
+document.getElementById('button-luffy').onclick = function(){
+    var myframe = document.getElementById('button-luffy');
+    myframe.style.backgroundColor = "green";
+};
+
+// zoro button
+document.getElementById('button-zoro').onclick = function(){
+    var myframe = document.getElementById('button-zoro');
+    myframe.style.backgroundColor = "red";
+    alert("Incorrect")
+};
+
+
+// lucky roo button
+document.getElementById('button-lucky-roo').onclick = function(){
+    var myframe = document.getElementById('button-lucky-roo');
+    myframe.style.backgroundColor = "red";
+    alert("Incorrect")
+};
+
+
+// sengoku button
+document.getElementById('button-sengoku').onclick = function(){
+    var myframe = document.getElementById('button-sengoku');
+    myframe.style.backgroundColor = "red";
+    alert("Incorrect")
+};
